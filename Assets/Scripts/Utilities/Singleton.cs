4529226f29ba10b5e7aab0022edc2fb0cd6e4ca9
saddansyah <saddanakbar@gmail.com>
2024-05-06 09:44:50 +0700

    init: project structures and in-scene hierarchy

---
+++ Assets/Scripts/Utilities/Singleton.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+/// <summary>
+/// Singleton -> Single-only static object but not persistent
+/// SingletonPersistent -> Single-only, static, and persistent accross scenes
+/// </summary>
+
+public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    public static T Instance { get; private set; }
+    protected virtual void Awake()
+    {
+        Instance = this as T;
+    }
+
+    protected void OnApplicationQuit()
+    {
+        Instance = null;
+        Destroy(gameObject);
+    }
+}
+
+public abstract class SingletonPersistent<T> : Singleton<T> where T : MonoBehaviour
+{
+    protected override void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+        base.Awake();
+    }
+}
