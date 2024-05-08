53da730c8541f6886c8b85cc28b2823c43cec5df
adityar22 <adityaramadhanarckarito47@gmail.com>
2024-05-08 19:55:51 +0700

    feat: init main game scene environtment

---
+++ Assets/_Scripts/Managers/DataUserManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataUserManager : MonoBehaviour
+{
+    void Start()
+    {
+        if (BaseSystem.Instance.userData != null) { }
+    }
+
+    
+
+    public void SaveGame()
+    {
+
+    }
+
+    public void LoadGame()
+    {
+
+    }
+}
--- Assets/_Scripts/Systems/BaseSystem.cs
+++ Assets/_Scripts/Systems/BaseSystem.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class BaseSystem : SingletonPersistent<BaseSystem>
 {
+    public User userData;
     public List<Level> listOfLevel;
     public List<Achievement> listOfAchievement;
 }
