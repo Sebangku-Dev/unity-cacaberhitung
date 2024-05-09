2ce0b08653a3d831073f3f9b7633555dec22f76b
saddansyah <saddanakbar@gmail.com>
2024-05-09 11:25:20 +0700

    feat: level manager

---
+++ Assets/Prefabs/Levels/Level1/Level1Gameplay.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level1Gameplay : BaseGameplay
+{
+    // Entire gameplay codebase here
+}
--- Assets/_Scripts/Managers/UserManager.cs
+++ Assets/_Scripts/Managers/UserManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DataManager : MonoBehaviour
+public class UserManager : MonoBehaviour
 {
     void Start()
     {
