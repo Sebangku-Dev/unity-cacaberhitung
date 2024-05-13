5438b69ef4df9c5b22444cf4b5b1eeb215125dc1
adityar22 <adityaramadhanarckarito47@gmail.com>
2024-05-13 21:59:04 +0700

    feat: boirplate location object

---
+++ Assets/_Scripts/Scriptables/Location.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Location", menuName ="Scriptable/Location")]
+public class Location : ScriptableObject
+{
+    public string locationName;
+    public string region;
+}
