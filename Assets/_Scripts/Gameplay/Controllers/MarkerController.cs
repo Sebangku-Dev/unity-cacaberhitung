2f51a53d8966c38f9ea9edd2791b21e6f7bfcec5
adityar22 <adityaramadhanarckarito47@gmail.com>
2024-05-14 13:33:16 +0700

    feat: add panel controller

--- Assets/_Scripts/Gameplay/Controllers/MarkerController.cs
+++ Assets/_Scripts/Gameplay/Controllers/MarkerController.cs
@@ -1,9 +1,39 @@
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MarkerController : MonoBehaviour
 {
+    public static bool isOnMarkerArea;
     [SerializeField] Level levelToPlay;
     // [SerializeField] NavigationSystem nav;
+
+    [SerializeField] TextMeshProUGUI TextTitle, TextDetail;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            TextTitle.text = levelToPlay.title;
+            TextDetail.text = levelToPlay.description;
+            
+            ToggleMissionPanel();
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            ToggleMissionPanel();
+        }
+    }
+
+    void ToggleMissionPanel()
+    {
+        NavigationSystem.Instance.TogglePanel(1);
+        MarkerController.isOnMarkerArea = !MarkerController.isOnMarkerArea;
+    }
 
     public void PlayLevel()
     {
--- Assets/_Scripts/Systems/NavigationSystem.cs
+++ Assets/_Scripts/Systems/NavigationSystem.cs
@@ -9,6 +9,8 @@ public class NavigationSystem : SingletonPersistent<NavigationSystem>
 {
     private static int lastScene;
 
+    [SerializeField] GameObject[] panels;
+
     public async void LoadScene(string targetScene)
     {
         lastScene = SceneManager.GetActiveScene().buildIndex;
@@ -32,5 +34,29 @@ public class NavigationSystem : SingletonPersistent<NavigationSystem>
     public void Back()
     {
         SceneManager.LoadScene(lastScene);
+    }
+
+    public void TogglePanel(int index)
+    {
+        if (panels != null)
+        {
+            int i = 0;
+            foreach (GameObject panel in panels)
+            {
+                if (i == index)
+                {
+                    panel.SetActive(!panel.activeSelf);
+                    if (index > 1)
+                    {
+                        panels[0].SetActive(panel.activeSelf == false);
+                        panels[1].SetActive(panel.activeSelf == false && MarkerController.isOnMarkerArea);
+                    }
+                    else if (index == 1) panels[0].SetActive(panel.activeSelf == false);
+                }
+                else panel.SetActive(false);
+
+                i++;
+            }
+        }
     }
 }
